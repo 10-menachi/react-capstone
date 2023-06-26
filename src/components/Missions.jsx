8af79d542a9fc7b0d54684ea531e3716246e48e2
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-06-26 19:12:06 +0100

    Fetch data for the missions

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,38 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import MissionDetails from './MissionDetails';
+import { fetchMissions } from '../redux/missions/missionsSlice';
+
 const Missions = () => {
+  const dispatch = useDispatch();
+  const { missions } = useSelector((store) => store.mission);
+
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
+
   return (
-    <div>
-      <h1>Missions</h1>
-    </div>
+    <table>
+      {missions.map((mission) => (
+        <MissionDetails
+          key={mission.mission_id}
+          name={mission.mission_name}
+          description={mission.description}
+        />
+      ))}
+    </table>
   );
+};
+
+Missions.propTypes = {
+  missions: PropTypes.arrayOf(
+    PropTypes.shape({
+      mission_id: PropTypes.string.isRequired,
+      mission_name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    })
+  ),
 };
 
 export default Missions;
