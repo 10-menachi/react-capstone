f4807d9074eb4e9a9cc86f635b0f78a2801a99de
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-06-26 13:15:35 +0100

    Create active route and view components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,2 +1,24 @@
 import { NavLink } from 'react-router-dom';
-import { useState } from 'react';
+import img from '../images/space-img.png';
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="head">
+        <img src={img} alt="Space-logo" />
+        <h1>Space Travelers&apos; Hub</h1>
+      </div>
+      <ul>
+        <li>
+          <NavLink to="/">Rockets</NavLink>
+        </li>
+        <li>
+          <NavLink to="missions">Missions</NavLink>
+        </li>
+        <li>
+          <NavLink to="myprofile">My profile</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
