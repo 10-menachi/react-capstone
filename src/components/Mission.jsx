04aa41c4a20f2b3b7db48b090232afade020b349
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-06-26 17:54:28 +0100

    Add Basic Structure for missions

---
+++ src/components/Mission.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from '../styles/Mission.module.css';
+
+const Mission = ({ mission }) => (
+  <tr>
+    <td className={styles.name}>{mission.name}</td>
+    <td className={styles.description}>{mission.description}</td>
+    <td>
+      <button type="button">Not a member</button>
+    </td>
+    <td>
+      <button type="button">Join mission</button>
+    </td>
+  </tr>
+);
+
+Mission.propTypes = {
+  mission: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Mission;
