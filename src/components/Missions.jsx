4a9d710b5ecc78d9982a4f54a47719fb5f5586c0
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-06-26 20:20:13 +0100

    Implement join and leave mission action

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,8 +1,30 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import MissionDetails from './MissionDetails';
+import { fetchMissions } from '../redux/missions/missionsSlice';
+
 const Missions = () => {
+  const dispatch = useDispatch();
+  const { missions } = useSelector((store) => store.missions);
+
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
+
   return (
-    <div>
-      <h1>Missions</h1>
-    </div>
+    <table>
+      <tbody>
+        {missions.map((mission) => (
+          <MissionDetails
+            key={mission.id}
+            name={mission.name}
+            description={mission.description}
+            id={mission.id}
+            reserved={mission.reserved}
+          />
+        ))}
+      </tbody>
+    </table>
   );
 };
 
