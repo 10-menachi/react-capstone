944bcf2c05f4bdf6b42c4515fe9154db3a3731bb
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-06-26 16:54:52 +0300

    Create empty header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import planet from "../assets/planet.png"
+
+const Header = () => {
+    return (
+        <nav>
+            <img src={planet} alt="" />
+        </nav>
+    )
+}
+
+export default Header
