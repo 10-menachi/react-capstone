ca22c47b4eeeeb8ebd00851c1607d1c3ff7c01f0
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-06-28 10:52:43 +0300

    Add Reserved Rockets in my profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Missions from './components/Missions';
 import MyProfile from './components/MyProfile';
 import Navbar from './components/Navbar';
 import Rockets from './components/Rockets';
+import { fetchRockets } from './redux/rockets/rocketSlice';
 
 const Layout = () => (
   <div>
@@ -12,16 +15,23 @@ const Layout = () => (
   </div>
 );
 
-const App = () => (
-  <div>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/" element={<Rockets />} />
-        <Route path="missions" element={<Missions />} />
-        <Route path="myprofile" element={<MyProfile />} />
-      </Route>
-    </Routes>
-  </div>
-);
+const App = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rockets.rockets);
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Rockets rockets={rockets} />} />
+          <Route path="missions" element={<Missions />} />
+          <Route path="myprofile" element={<MyProfile rockets={rockets} />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,8 +1,38 @@
-const MyProfile = () => (
-  <div className="profile">
-    <h1>My Profile</h1>
-    <p>Some text about me...</p>
-  </div>
-);
+import PropTypes from 'prop-types';
+
+const MyProfile = ({ rockets }) => {
+  let borTop = '';
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved === true);
+  return (
+    <div className="profile">
+      <div className="missions-prof">
+        <h2>My Missions</h2>
+      </div>
+      <div className="rockets-prof">
+        <h2>My Rockets</h2>
+        {reservedRockets.length === 0 ? <p className="no-reserve">No reserved rockets</p> : reservedRockets.map((rocket) => {
+          borTop = rocket.id === 1 ? 'top' : '';
+          return (
+            <div className={`rocket-prof ${borTop}`} key={rocket.id}>
+              <p>{rocket.rocket_name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+MyProfile.propTypes = {
+  rockets: PropTypes.arrayOf(PropTypes.shape(
+    {
+      rocket_id: PropTypes.string.isRequired,
+      flickr_images: PropTypes.arrayOf(PropTypes.string).isRequired,
+      rocket_name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      reserved: PropTypes.bool.isRequired,
+    },
+  )).isRequired,
+};
 
 export default MyProfile;
